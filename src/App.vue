046ecf539165e1d95721f3f0f814086f502e95c7
hollis <243184473+cathollis@users.noreply.github.com>
2025-11-12 01:51:13 +0800

    fix: nav area display

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import { shallowRef } from 'vue'
 import { menuList } from './router'
+
+const isNavOpen = shallowRef(true)
+
+const showNav = () => {
+  isNavOpen.value = true
+}
 </script>
 
 <template>
   <v-responsive class="border rounded">
     <v-app>
-      <v-app-bar title="Toolbox"></v-app-bar>
+      <v-app-bar title="Toolbox">
+        <template v-slot:prepend>
+          <v-btn @click="showNav"> 显示导航栏 </v-btn>
+        </template>
+      </v-app-bar>
 
-      <v-navigation-drawer>
+      <v-navigation-drawer v-model="isNavOpen">
         <v-list v-for="menuItem in menuList">
           <v-list-item :to="menuItem.path" :title="menuItem.title"></v-list-item>
         </v-list>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import App from './App.vue'
 import router from './router'
 
@@ -13,6 +15,13 @@ const app = createApp(App)
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 app.use(router)
