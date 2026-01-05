bf019e740d23fee01bfc525eafe37150fb9a285d
hollis <243184473+cathollis@users.noreply.github.com>
2026-01-05 23:11:58 +0800

    feat: update migration

--- toolbox-backend/Hollis.Toolbox/Hollis.Toolbox.Functions/Migrations/20260105150730_AlterTablePastebinItemAddConfiguredExpiredAfterRead.Designer.cs
+++ toolbox-backend/Hollis.Toolbox/Hollis.Toolbox.Functions/Migrations/20260105150730_AlterTablePastebinItemAddConfiguredExpiredAfterRead.Designer.cs
@@ -4,6 +4,7 @@ using Hollis.Toolbox.Functions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hollis.Toolbox.Functions.Migrations
 {
     [DbContext(typeof(ToolboxDbContext))]
-    partial class ToolboxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260105150730_AlterTablePastebinItemAddConfiguredExpiredAfterRead")]
+    partial class AlterTablePastebinItemAddConfiguredExpiredAfterRead
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ toolbox-backend/Hollis.Toolbox/Hollis.Toolbox.Functions/Migrations/20260105150730_AlterTablePastebinItemAddConfiguredExpiredAfterRead.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hollis.Toolbox.Functions.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTablePastebinItemAddConfiguredExpiredAfterRead : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfiguredExpiredAfterRead",
+                schema: "Toolbox",
+                table: "PastebinItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfiguredExpiredAfterRead",
+                schema: "Toolbox",
+                table: "PastebinItems");
+        }
+    }
+}
